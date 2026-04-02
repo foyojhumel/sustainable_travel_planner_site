41e6316514c07edef022f8862c1cb5f8623d9420
foyojhumel <foyojhumel24@gmail.com>
2026-04-02 09:24:58 +0800

    Added design of search bar and initial design for tourist suggestion section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,25 @@ module.exports = {
       colors: {
         "surface": "#fafaf5",
         "on-surface": "#1a1c19",
+        "primary-container": "#0047ab",
+        "on-primary-container": "a5bdff",
+        "primary": "#00327d",
+        "on-primary": "#ffffff",
+        "on-surface-variant": "#434653",
+        "secondary": "#1b6d24",
+        "outline-variant": "#c3c6d5",
+        "surface-container-high": "#e8e8e3",
       },
       fontFamily: {
-        body: ["Manrope"],
-      }
+        "headline": ["Plus Jakarta Sans"],
+        "body": ["Manrope"],
+      },
+      borderRadius: {
+        "DEFAULT": "0.25rem",
+        "lg": "0.5rem",
+        "xl": "0.75rem",
+        "full": "9999px",
+      },
     },
   },
   plugins: [],
